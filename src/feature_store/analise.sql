3aac55238e8d2754577019abcd56db7beb626ef4
Gustavo Weingaertner <83656797+gustweing@users.noreply.github.com>
2024-06-16 15:08:30 -0300

    Delete src directory

--- src/feature_store/analise.sql
+++
@@ -1,9 +0,0 @@
--- Databricks notebook source
-select dtRef,
-       count(*),
-       count(distinct idPlayer)
-
-from silver_gc.fs_assinatura
-
-group by 1
-order by 1
--- src/tempCodeRunnerFile.sh
+++
@@ -1,4 +0,0 @@
-cd feature_store
-bash exec.sh
-cd ../predict
-python predict.py
